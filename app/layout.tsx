5ed8d7a7d5f352c1f61a3c0e783f04458309a5db
Kirill Ilin <k.ilin2003@yandex.ru>
2023-07-28 16:30:10 +0300

    another one5.3.4.32

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
             <body>
                 <Navbar />
                 <main>
-                    {children}
+                    
                 </main>
                 <Footer />
             </body>
