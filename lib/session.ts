a58684c54dcf325a168b00a2683e4dedbd1d891f
Kirill Ilin <k.ilin2003@yandex.ru>
2023-07-28 16:12:47 +0300

    another one5.3.4.27

--- lib/session.ts
+++ lib/session.ts
@@ -14,20 +14,20 @@ export const authOptions: NextAuthOptions = {
             clientSecret: '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'
         })
     ],
-    jwt: {
-        encode: ({ secret, token }) => {
-            const encodedToken = jsonwebtoken.sign({
-                ...token,
-                iss: 'grafbase',
-                exp: Math.floor(Date.now() / 1000) + 60 * 60
-            }, secret);
-            return encodedToken;
-        },
-        decode: async ({ secret, token }) => {
-            const decodedToken = jsonwebtoken.verify(token!, secret) as JWT;
-            return decodedToken;
-        }
-    },
+    // jwt: {
+    //     encode: ({ secret, token }) => {
+    //         const encodedToken = jsonwebtoken.sign({
+    //             ...token,
+    //             iss: 'grafbase',
+    //             exp: Math.floor(Date.now() / 1000) + 60 * 60
+    //         }, secret);
+    //         return encodedToken;
+    //     },
+    //     decode: async ({ secret, token }) => {
+    //         const decodedToken = jsonwebtoken.verify(token!, secret) as JWT;
+    //         return decodedToken;
+    //     }
+    // },
     theme: {
         colorScheme: 'dark',
         logo: 'logo.svg'
