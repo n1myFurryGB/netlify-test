3059eea922caf59d3c5a64610607f364839dcdb6
Kirill Ilin <k.ilin2003@yandex.ru>
2023-07-28 16:56:21 +0300

    another one5.3.4.40

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,10 @@
-import { NavLinks } from '@/constant';
-import Image from 'next/image';
-import Link from 'next/link';
-import React from 'react';
-import AuthProviders from './AuthProviders';
+// import { NavLinks } from '@/constant';
+// import Image from 'next/image';
+// import Link from 'next/link';
+// import React from 'react';
+// import AuthProviders from './AuthProviders';
+// import ProfileMenu from './ProfileMenu';
 import { authConfig } from '@/lib/session';
-import ProfileMenu from './ProfileMenu';
 import { getServerSession } from 'next-auth/next';
 
 const Navbar = async () => {
@@ -17,39 +17,39 @@ const Navbar = async () => {
         </div>
     )
 
-  return (
-    <nav className="flexBetween navbar">
-        <div className="flex-1 flexStart gap-10">
-            <Link href="/">
-                <Image
-                    src="/logo.svg"
-                    width={115}
-                    height={43}
-                    alt="Flexibble"
-                />
-            </Link>
-            <ul className="xl:flex hidden text-small gap-7">
-                {NavLinks.map((link) => (
-                    <Link href={link.href} key={link.key}>
-                        {link.text}
-                    </Link>
-                ))}
-            </ul>
-        </div>
-        <div className="flexCenter gap-4">
-            {/* {session?.user ? (
-                <>
-                    <ProfileMenu session={session} />
-                    <Link href="/create-project">
-                        Share Work
-                    </Link>
-                </>
-            ) : (
-                <AuthProviders />
-            )} */}
-        </div>
-    </nav>
-  )
+//   return (
+//     <nav className="flexBetween navbar">
+//         <div className="flex-1 flexStart gap-10">
+//             <Link href="/">
+//                 <Image
+//                     src="/logo.svg"
+//                     width={115}
+//                     height={43}
+//                     alt="Flexibble"
+//                 />
+//             </Link>
+//             <ul className="xl:flex hidden text-small gap-7">
+//                 {NavLinks.map((link) => (
+//                     <Link href={link.href} key={link.key}>
+//                         {link.text}
+//                     </Link>
+//                 ))}
+//             </ul>
+//         </div>
+//         <div className="flexCenter gap-4">
+//             {/* {session?.user ? (
+//                 <>
+//                     <ProfileMenu session={session} />
+//                     <Link href="/create-project">
+//                         Share Work
+//                     </Link>
+//                 </>
+//             ) : (
+//                 <AuthProviders />
+//             )} */}
+//         </div>
+//     </nav>
+//   )
 }
 
 export default Navbar
